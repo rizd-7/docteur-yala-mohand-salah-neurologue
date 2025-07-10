f26b98cd7a0c8ed67b07b9fa3c4d7e78b7bf40b5
riad <riadyala79@gmail.com>
2025-07-10 14:51:05 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // eslint-disable-next-line no-undef
+  base: process.env.VITE_BASE_PATH ||  "/docteur-yala-mohand-salah-neurologue"
 });
